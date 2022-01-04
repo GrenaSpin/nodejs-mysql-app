d0bef2aaf4ea789fd57439cd203aec4ca142b6dc
Yael López Gómez <yaellopezgomez2013@gmail.com>
2022-01-03 19:17:48 -0600

    configure my script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const { engine } = require('express-handlebars');
 const path = require('path');
 const flash  = require('connect-flash');
